7ef4e9c1e0d015e5ec955126e0b1379bd348e78a
Hagyeong Kim <78262460+hhagyeong@users.noreply.github.com>
2021-02-06 22:56:46 +0900

    Fix: 오탈자 수정

--- WebMarket/WebContent/resources/js/validation.js
+++ WebMarket/WebContent/resources/js/validation.js
@@ -1,16 +1,17 @@
-funtion CheckAddProduct(){
+function CheckAddProduct() {
 	var productId = document.getElementById("productId");
-	var name = document.getElementById("productName");
+	var name = document.getElementById("name");
 	var unitPrice = document.getElementById("unitPrice");
 	var unitsInStock = document.getElementById("unitsInStock");
 
 	//상품 아이디 체크: P로 시작하고 숫자를 조합, 문자 길이 5~12
-	if (!check(/^P[0-9]{4,11}$/  productId, "[상품 코드]\nP와 숫자를 조합하여 5~12자까지 입력하세요\n첫 글자는 반드시 P로 시작하세요"))
+	if (!check(/^P[0-9]{4,11}$/, productId,
+		"[상품 코드]\nP와 숫자를 조합하여 5~12자까지 입력하세요\n첫 글자는 반드시 P로 시작하세요"))
 		return false;
 
 	//상품명 체크: 문자 길이 4~12
 	if (name.value.length < 4 || name.value.length > 12) {
-		alert("[상품명]\n최소 4자에서 최대 12자까지 입력하세요");
+		alert("[상품명]\n최소 4자에서 최대 50자까지 입력하세요");
 		name.select();
 		name.focus();
 		return false;
@@ -23,13 +24,13 @@ funtion CheckAddProduct(){
 		return false;
 	}
 	//음수인지 확인
-	if (unitPrice.value < 0  {
-		alert("[가격]\n음수는 입력할 수 없습니다");
+	if (unitPrice.value < 0) {
+		alert("[가격]\n음수를 입력할 수 없습니다");
 		unitPrice.select();
 		unitPrice.focus();
 		return false;
-		//숫자, 소수점 둘째 자리까지 가능
-	} else if (!check(/^\d+(?:[.]?[\d])?$/, unitPrice, "[기갹]\n소수점 둘째 자리까지만 입력하세요"))
+	} else if (!check(/^\d+(?:[.]?[\d]?[\d])?$/, unitPrice,
+		"[가격]\n소수점 둘째 자리까지만 입력하세요"))
 		return false;
 
 	//재고 수 체크: 숫자가 아닌지 확인
@@ -40,10 +41,11 @@ funtion CheckAddProduct(){
 		return false;
 	}
 	function check(regExp, e, msg) {
-		if (regExp.text(e.value)) {
+
+		if (regExp.test(e.value)) {
 			return true;
 		}
-		laert(msg);
+		alert(msg);
 		e.select();
 		e.focus();
 		return false;
