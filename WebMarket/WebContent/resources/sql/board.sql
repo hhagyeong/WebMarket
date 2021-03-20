9f1522fdb316e0ec529033570434d09c3b262ec9
Hagyeong Kim <78262460+hhagyeong@users.noreply.github.com>
2021-03-20 23:39:18 +0900

    Feat: 주석 추가

--- WebMarket/WebContent/resources/sql/board.sql
+++ WebMarket/WebContent/resources/sql/board.sql
@@ -1,11 +1,11 @@
 CREATE TABLE board (
-   num int not null auto_increment,
-   id varchar(10) not null,
-   name varchar(10) not null,
-   subject varchar(100) not null,
-   content text not null,
-   regist_day varchar(30),
-   hit int,
-   ip varchar(20),
-   PRIMARY KEY (num)
+   num int not null auto_increment, --게시글 순번
+   id varchar(10) not null, --회원 아이디
+   name varchar(10) not null, --회원 이름
+   subject varchar(100) not null, --게시글 제목
+   content text not null, --게시글 내용
+   regist_day varchar(30), --게시글 등록 일자
+   hit int, --게시글 조회 수
+   ip varchar(20), --게시글 등록 시 IP
+   PRIMARY KEY (num) --게시글 순번을 고유 키로 설정
 )default CHARSET=utf8;
